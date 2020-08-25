10357df64436be666eb434667b893aa1879bd7d1
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-08-25 04:57:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,41 @@ import TodoItem from './TodoItem';
 import todoData from './todoData';
 import './App.css';
 
-function App() {
-const todoItems = todoData.map(item => <TodoItem key={item.id} item={item}/>)
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todoData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-  return (
-    <div className="todo-list">
-    {todoItems}
-   </div>
-  );
-}
+  handleChange(id) {
+  this.setState(prevState => {
+    const updatedTodos = prevState.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    return {
+      todos: updatedTodos
+    }
+  })
+   
+  }
+  render() {
+    const todoItems = todoData.map(item => <TodoItem key={item.id} item={item} 
+      handleChange={this.handleChange}
+    />)
+
+    return (
+      <div className="todo-list">
+      {todoItems}
+     </div>
+    );
+  }
+  }
+
 
 export default App;
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+
 function TodoItem(props) {
-    return(
-        <div className="todo-list">
-         <input type="checkbox" checked={props.item.completed} />
-         <p>{props.item.text}</p>
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+      }
+     return(
+        <div className="todo-item">
+         <input type="checkbox" checked={props.item.completed} 
+         onChange={() => props.handleChange(props.item.id)} />
+         <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
 
         </div>
     );
